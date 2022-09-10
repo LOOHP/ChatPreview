25c95697b8f32f02683f9e5d37b0b95f6f51f2f9
LOOHP <jamesloohp@gmail.com>
2022-09-10 13:38:28 +0800

    1.19.2

--- src/main/java/com/loohp/chatpreview/ChatPreview.java
+++ src/main/java/com/loohp/chatpreview/ChatPreview.java
@@ -1,29 +1,24 @@
 package com.loohp.chatpreview;
 
-import com.comphenix.protocol.PacketType;
-import com.comphenix.protocol.ProtocolLibrary;
-import com.comphenix.protocol.events.ListenerPriority;
-import com.comphenix.protocol.events.PacketAdapter;
-import com.comphenix.protocol.events.PacketContainer;
-import com.comphenix.protocol.events.PacketEvent;
-import com.comphenix.protocol.wrappers.WrappedChatComponent;
 import me.clip.placeholderapi.PlaceholderAPI;
 import net.md_5.bungee.api.ChatColor;
-import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatPreviewEvent;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
-public class ChatPreview extends JavaPlugin {
+public class ChatPreview extends JavaPlugin implements Listener {
 
     public static final Pattern COLOR_PATTERN = Pattern.compile("&#([0-9a-fA-F])([0-9a-fA-F])([0-9a-fA-F])([0-9a-fA-F])([0-9a-fA-F])([0-9a-fA-F])");
 
@@ -40,62 +35,37 @@ public class ChatPreview extends JavaPlugin {
         loadConfig();
 
         getCommand("chatpreviewreload").setExecutor(this);
-
-        ProtocolLibrary.getProtocolManager().addPacketListener(new PacketAdapter(PacketAdapter.params().plugin(plugin).listenerPriority(ListenerPriority.MONITOR).types(PacketType.Login.Client.START, PacketType.Play.Client.CHAT_PREVIEW)) {
-            @Override
-            public void onPacketReceiving(PacketEvent event) {
-                if (event.isPlayerTemporary() || event.isCancelled()) {
-                    return;
-                }
-
-                PacketType packetType = event.getPacketType();
-                Player player = event.getPlayer();
-                PacketContainer packet = event.getPacket();
-
-                if (packetType.equals(PacketType.Login.Client.START)) {
-                    packet.getModifier().write(1, Optional.empty());
-                } else if (packetType.equals(PacketType.Play.Client.CHAT_PREVIEW)) {
-                    int id = packet.getIntegers().read(0);
-                    String chat = packet.getStrings().read(0);
-                    Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
-                        PacketContainer previewPacket = ProtocolLibrary.getProtocolManager().createPacket(PacketType.Play.Server.CHAT_PREVIEW);
-                        previewPacket.getIntegers().write(0, id);
-
-                        String chatMessage = chat;
-                        String format = chatFormat;
-                        if (chat.startsWith("/")) {
-                            for (Entry<String, String> entry : commandFormats.entrySet()) {
-                                String command = entry.getKey();
-                                if (chat.startsWith(command)) {
-                                    format = entry.getValue();
-                                    chatMessage = chatMessage.substring(command.length());
-                                    if (chatMessage.charAt(0) == ' ') {
-                                        chatMessage = chatMessage.substring(1);
-                                    }
-                                    break;
-                                }
-                            }
-                        }
-
-                        if (hasInteractiveChat()) {
-                            chatMessage = InteractiveChatHook.translateColors(player, chatMessage);
-                        }
-
-                        String result = translateAlternateColorCodes('&', PlaceholderAPI.setPlaceholders(player, format)).replace("{Message}", chatMessage);
-                        previewPacket.getChatComponents().write(0, WrappedChatComponent.fromLegacyText(result));
-
-                        ProtocolLibrary.getProtocolManager().sendServerPacket(player, previewPacket);
-                    });
-                }
-            }
-
-            @Override
-            public void onPacketSending(PacketEvent event) {
-                //do nothing
-            }
-        });
+        getServer().getPluginManager().registerEvents(this, this);
 
         getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "ChatPreview has been enabled!");
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onChatPreview(AsyncPlayerChatPreviewEvent event) {
+        Player player = event.getPlayer();
+        String chat = event.getMessage();
+        String chatMessage = chat;
+        String format = chatFormat;
+        if (chat.startsWith("/")) {
+            for (Entry<String, String> entry : commandFormats.entrySet()) {
+                String command = entry.getKey();
+                if (chat.startsWith(command)) {
+                    format = entry.getValue();
+                    chatMessage = chatMessage.substring(command.length());
+                    if (chatMessage.charAt(0) == ' ') {
+                        chatMessage = chatMessage.substring(1);
+                    }
+                    break;
+                }
+            }
+        }
+
+        if (hasInteractiveChat()) {
+            chatMessage = InteractiveChatHook.translateColors(player, chatMessage);
+        }
+
+        String result = translateAlternateColorCodes('&', PlaceholderAPI.setPlaceholders(player, format)).replace("{Message}", chatMessage);
+        event.setMessage(result);
     }
 
     public void loadConfig() {
